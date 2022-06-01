5781328d56a98d18ecf4e5f30710e0356aa52b3d
sandeepsinghvirdi <sandeep_sonu07@yahoo.com>
2022-06-01 20:12:53 +1000

    Added code to pass the tests

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Grace Hoper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+//const shortGreeting = 'n Welcome, !';
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
